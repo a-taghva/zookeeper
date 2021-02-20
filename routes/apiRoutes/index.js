c1750416b479e14e7d5690d5f38a8f12acb6820b
a-taghva <taghvali99@gmail.com>
2021-02-19 23:49:23 -0500

    add zookeepersRoute

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const animalRoutes = require('./animalRoutes.js');
+const zookeepersRoutes = require('./zookeepersRoutes');
 
 router.use(animalRoutes);
+router.use(zookeepersRoutes);
 
 module.exports = router;
